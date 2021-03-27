82566bb40b3b5728c98417e2b63292225967fd08
Mario Cabrera <yankeescab@gmail.com>
2021-03-27 13:45:52 -0400

    got the search bar to properly take input data

--- Assets/script.js
+++ Assets/script.js
@@ -1,15 +1,16 @@
-var searchBar = document.getElementById("#citySearchBar");
-var cityName = searchBar.value();
-var goBtn = document.getElementById('#citySearchBtn');
+var formEl = $('#searchForm');
+var cityInputEl = $('input[name="cityName"]');
+var searchBtn = document.getElementById('citySearchBtn');
 
-// fetch('https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=06ac0e5e9aecb8b74a0ca04ef150a8a4')
-// .then(Response => Response.json())
-// .then(data => console.log(data));
-
-
-fetch('https://api.opencagedata.com/geocode/v1/json?q=Atlanta&key=1f2ccb5ef25046768295cd37fe62ae32&language=en&pretty=1')
+fetch('http://api.openweathermap.org/data/2.5/weather?q=Houston&appid=06ac0e5e9aecb8b74a0ca04ef150a8a4')
 .then(Response => Response.json())
 .then(data => console.log(data));
 
-goBtn.addEventListener("click")
-    console.log(cityName);
+
+
+function handleFormSubmit(event) {
+event.preventDefault();
+console.log(cityInputEl.val());
+}
+
+searchBtn.addEventListener("click", handleFormSubmit);
