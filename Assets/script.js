7c5b477417fa1106db385786c7a02bcf3b896cc4
Mario Cabrera <yankeescab@gmail.com>
2021-03-27 14:46:03 -0400

    continuing to get all of the API data implinented into the page. From here on until said otherwise, will denote this same action by saying 'Cont...'

--- Assets/script.js
+++ Assets/script.js
@@ -1,16 +1,27 @@
 var formEl = $('#searchForm');
 var cityInputEl = $('input[name="cityName"]');
 var searchBtn = document.getElementById('citySearchBtn');
-
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Houston&appid=06ac0e5e9aecb8b74a0ca04ef150a8a4')
-.then(Response => Response.json())
-.then(data => console.log(data));
-
+var openWeatherFirst = 'http://api.openweathermap.org/data/2.5/weather?q='
+var openWeatherLast = '&appid=06ac0e5e9aecb8b74a0ca04ef150a8a4&units=imperial'
+var currentCityName = $('#mainWeatherHeader');
 
 
 function handleFormSubmit(event) {
+
 event.preventDefault();
-console.log(cityInputEl.val());
+fetch(openWeatherFirst + cityInputEl.val() + openWeatherLast)
+.then(Response => Response.json())
+.then(data => console.log(data));
+insertMainData();
+
 }
 
 searchBtn.addEventListener("click", handleFormSubmit);
+
+function insertMainData(){
+document.getElementById('mainWeatherHeader').innerHTML = cityInputEl.val();
+    
+}
+
+
+
