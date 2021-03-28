ad53f792f9112b4d3cc6a9df8759f3bdedada1d6
Mario Cabrera <yankeescab@gmail.com>
2021-03-27 22:36:31 -0400

    sucessfully able to take API data and load it into HTML doc

--- Assets/script.js
+++ Assets/script.js
@@ -1,27 +1,75 @@
+// Variables
+
 var formEl = $('#searchForm');
 var cityInputEl = $('input[name="cityName"]');
 var searchBtn = document.getElementById('citySearchBtn');
-var openWeatherFirst = 'http://api.openweathermap.org/data/2.5/weather?q='
-var openWeatherLast = '&appid=06ac0e5e9aecb8b74a0ca04ef150a8a4&units=imperial'
+var currentWeatherFirst = 'http://api.openweathermap.org/data/2.5/weather?q='
+var allWeatherLast = '&appid=06ac0e5e9aecb8b74a0ca04ef150a8a4&units=imperial'
+var futureWeatherFirst = 'http://api.openweathermap.org/data/2.5/forecast?q='
 var currentCityName = $('#mainWeatherHeader');
+var d = new Date();
+var date = d.toLocaleDateString();
 
+// Button to start everything
+
+searchBtn.addEventListener("click", handleFormSubmit);
+
+//Function to pull all API and set basic data
 
 function handleFormSubmit(event) {
 
 event.preventDefault();
-fetch(openWeatherFirst + cityInputEl.val() + openWeatherLast)
+pullCurrentData();
+pullFutureData();
+applyNameAndTime();
+}
+
+// Pull Current API Data
+
+function pullCurrentData(){
+    fetch(currentWeatherFirst + cityInputEl.val() + allWeatherLast)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    
+        const temp = data.main.temp;
+        const humidity = data.main.humidity;
+        const windSpeed = data.wind.speed;
+
+        console.log(temp);
+        console.log(humidity);
+        console.log(windSpeed);
+
+    function insertDataToCurrent(){
+        document.getElementById('tempText').innerHTML = "Temperature: " + temp + " F";
+        document.getElementById('humidityText').innerHTML = "Humidity: " + humidity;
+        document.getElementById('windSpeedText').innerHTML = "Wind Speeds: " + windSpeed + " mph";
+        }
+
+        insertDataToCurrent();
+        
+    })}
+
+
+
+
+
+
+// Pull future API Data
+
+function pullFutureData(){
+
+fetch(futureWeatherFirst + cityInputEl.val() + allWeatherLast)
 .then(Response => Response.json())
 .then(data => console.log(data));
-insertMainData();
-
 }
 
-searchBtn.addEventListener("click", handleFormSubmit);
+//Function to set the time and current city
 
-function insertMainData(){
-document.getElementById('mainWeatherHeader').innerHTML = cityInputEl.val();
+function applyNameAndTime(){
+    document.getElementById('mainWeatherHeader').innerHTML = cityInputEl.val() + " " + '(' + date + ')';
     
 }
-
-
 
